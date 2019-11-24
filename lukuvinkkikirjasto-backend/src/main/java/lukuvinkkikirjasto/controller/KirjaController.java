bf02a86f7b16b0e417133dd653d903dcc9ae0224
Yusif Salam-zade <yusalam@gmail.com>
2019-11-24 22:11:19 +0200

    change controller annotation

--- lukuvinkkikirjasto-backend/src/main/java/lukuvinkkikirjasto/controller/KirjaController.java
+++ lukuvinkkikirjasto-backend/src/main/java/lukuvinkkikirjasto/controller/KirjaController.java
@@ -18,11 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author salojuur
- */
-@RestController
+
+@Controller
 public class KirjaController {
     
     @Autowired
