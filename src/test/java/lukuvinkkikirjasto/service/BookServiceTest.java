66c1b0f81ef986041878ecbd3df333264a078397
Jari Salojuuri <jari.salojuuri@gmail.com>
2019-12-09 21:41:41 +0200

    add jacocoTestReport to .circleci/config.yml test run list

--- src/test/java/lukuvinkkikirjasto/service/BookServiceTest.java
+++ src/test/java/lukuvinkkikirjasto/service/BookServiceTest.java
@@ -1,18 +1,61 @@
 package lukuvinkkikirjasto.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import lukuvinkkikirjasto.entity.Book;
+import lukuvinkkikirjasto.repository.BookRepository;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+//@RunWith(SpringRunner.class)
 public class BookServiceTest {
+    
+    /*
+    @TestConfiguration
+    public class BookServiceTestContextConfiguration {
+        
+        @Bean
+        public BookService getBookService() {
+            return new BookService();
+        }
+    }
 
+    @Autowired
+    private BookService bookService;
+    
+    @MockBean
+    private BookRepository bookRepository;
+    
+    private Book book;
+    
     @Before
     public void setUp() throws Exception {
-
+        String author = "Robert Martin";
+        String title = "Clean Code";
+        String isbn = "978-0132350884";
+        book = new Book();
+        book.setAuthor(author);
+        book.setTitle(title);
+        book.setIsbn(isbn);
+        
+        Mockito.when(bookRepository.findAll().get(0).getAuthor()).thenReturn("Robert Martin");
+        
     }
     
     @Test
-    public void constructorWorks() {
-       
+    public void validAuthorIsReturned() {
+        assertEquals("Robert Martin", bookService.getBooks().get(0).getAuthor());
     }
+    */
 }
 
